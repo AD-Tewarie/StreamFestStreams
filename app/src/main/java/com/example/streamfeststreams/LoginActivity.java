dfebd8b3725cb47898d3eb0bfa724f0ba82228ca
SemWenders <9JmZGguepu955uM>
2020-06-25 11:31:05 +0200

    login page darkmode

--- app/src/main/java/com/example/streamfeststreams/LoginActivity.java
+++ app/src/main/java/com/example/streamfeststreams/LoginActivity.java
@@ -3,24 +3,41 @@ package com.example.streamfeststreams;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText Name;
     private EditText Password;
     private Button Login;
     SharedPreferences sp;
-
+    ConstraintLayout layout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+
+        layout = (ConstraintLayout)findViewById(R.id.login_layout_constraint);
+        if (isDarkModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            layout.setBackgroundResource(R.drawable.login_donker);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            layout.setBackgroundResource(R.drawable.login_licht);
+        }
 
         Name = (EditText) findViewById(R.id.nameLogin);
         Password = (EditText) findViewById(R.id.passLogin);
